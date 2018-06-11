df7a87b3e0a1dafc2e45cc8cea962ed578a2a4d7
Tobias Zeumer <github@verweisungsform.de>
2018-06-11 22:11:02 +0200

    Add setup.py and upload to https://pypi.org/manage/project/sip2/releases/. References #3

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup (
+    name='Sip2',
+    version='1.0',
+    description='SIP2 Python Client: Simple Interchange Protocol Client for Python',
+    long_description=open('README.md').read(),
+    author='Tobias Zeumer',
+    author_email='tzeumer@verweisungsform.de',
+    url='https://github.com/tzeumer/SIP2-Client-for-Python',
+    license='MIT',
+    packages=['Sip2'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
